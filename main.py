397df3df3e422c9dd619acbb36049e0995b886c8
dillongeary <13gearyd@gmail.com>
2023-08-09 20:03:38 +0100

    altered script to pull ical from www.southampton.gov.uk instead of from local file

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 import asyncio
 import icalendar
 import discord
-from datetime import datetime, timedelta
+import requests
+import re
+from datetime import datetime, timedelta, time,date
 
 from discord.ext import tasks
 
@@ -13,11 +15,40 @@ class Bin:
         self.date = date
 
 
-def setUp():
+def getMoreBinDates():
+    params = {
+        'UPRN': 100060676634,
+    }
+
+    response = requests.get('https://www.southampton.gov.uk/whereilive/waste-calendar', params=params)
+
+    ufprt = ""
+
+    for lin in response.text.splitlines():
+        if "ufprt" in lin:
+            ufprt = re.search(r'(?<=value=").+(?=")', lin)
+            break
+
+    print(ufprt.group(0))
+
+    data = {
+        'ufprt': ufprt.group(0),
+    }
+    response2 = requests.post('https://www.southampton.gov.uk/whereilive/waste-calendar', params=params, data=data)
+    return setUp(ical=response2.content)
+
+
+def readICALFromFile():
+    e = open("bins.ics", "rb")
+    output = setUp(e.read())
+    e.close()
+    return output
+
+
+def setUp(ical):
     # Use a breakpoint in the code line below to debug your script.
     binObjects = []
-    e = open("bins.ics", "rb")
-    ecal = icalendar.Calendar.from_ical(e.read())
+    ecal = icalendar.Calendar.from_ical(ical)
     for component in ecal.walk():
         if component.name == "VEVENT":
             binType = component.get("SUMMARY")
@@ -28,34 +59,38 @@ def setUp():
                     binPrint = binType + " BIN"
             binDate = component.decoded("dtstart")
             binObjects.append(Bin(binPrint, binDate))
-    e.close()
 
     return binObjects
 
 
+timeToRun = time(hour=19, minute=51)
+
 @tasks.loop(hours=24)
 async def checkBins(client, bins):
     print("running")
+
+    breakflag = True
     for bin in bins:
         if bin.date == datetime.now().date() + timedelta(0):
             channel = client.get_channel(628710600071053314)
             await channel.send("REMINDER : PUT OUT " + bin.bin + "!")
+        if bin.date > datetime.now().date():
+            breakflag = False
+    if breakflag:
+        checkBins.cancel()
 
+@checkBins.after_loop
+async def after_task():
+    bins = getMoreBinDates()
+    checkBins.start(client, bins)
 
-# Press the green button in the gutter to run the script.
-
-
-token = open(".token","r").read()
-intents = discord.Intents.default()
-
-client = discord.Client(intents=intents)
 
 
 @checkBins.before_loop
 async def before_task():
     print("now")
-    hour = 00
-    minute = 47
+    hour = 19
+    minute = 59
     now = datetime.now()
     print(now)
     future = datetime(now.year, now.month, now.day, hour, minute)
@@ -67,12 +102,16 @@ async def before_task():
     await asyncio.sleep((future - now).seconds)
 
 
+token = open(".token", "r").read()
+intents = discord.Intents.default()
+
+client = discord.Client(intents=intents)
+
+
 @client.event
 async def on_ready():
-    bins = setUp()
+    bins = getMoreBinDates()
     checkBins.start(client, bins)
 
 
 client.run(token)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
